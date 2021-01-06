48694c86a48027fb54cdef9e6a1e385cd177e374
Astrapel Lygdamus <astrapel@protonmail.ch>
2021-01-06 14:56:12 +0300

    prefix only /

--- genelmudur/utils/prefix.py
+++ genelmudur/utils/prefix.py
@@ -2,4 +2,4 @@ from functools import partial
 from pyrogram import filters
 
 # Change prefix only from here and add from here.
-cmdprefix = partial(filters.command, prefixes=["!", "/", "."])
+cmdprefix = partial(filters.command, prefixes=["/"])
