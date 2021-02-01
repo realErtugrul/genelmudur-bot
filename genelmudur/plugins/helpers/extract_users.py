e9e639761b0b6aa52843d9d22321be812c5d7dcb
realErtugrul <realertugrul@protonmail.com>
2021-02-02 00:43:22 +0100

    Update extract_users.py

--- genelmudur/plugins/helpers/extract_users.py
+++ genelmudur/plugins/helpers/extract_users.py
@@ -3,22 +3,26 @@ from typing import List
 from pyrogram.types import Message, User
 
 
-async def extract_users(msg: Message) -> List[User]:
+async def extract_users(message: Message) -> List[User]:
     total: List[User] = []
 
-    if msg.reply_to_message and msg.reply_to_message.from_user:
-        total.append(msg.reply_to_message.from_user)
+    if message.reply_to_message and message.reply_to_message.from_user:
+        total.append(message.reply_to_message.from_user)
 
     if (
-        len(mentions := [entity for entity in msg.entities if "mention" in entity.type])
+        len(
+            mentions := [
+                entity for entity in message.entities if "mention" in entity.type
+            ]
+        )
         > 0
     ):
         no_username = [i.user for i in mentions if i.type == "text_mention"]
         total += no_username
 
         async def get_member(off: int, len: int) -> User:
-            mention = msg.text[off : off + len]
-            return await msg._client.get_users(user_ids=mention)
+            mention = message.text[off : off + len]
+            return await message._client.get_users(user_ids=mention)
 
         possible_users = [i for i in mentions if i.type == "mention"]
         for nickname in possible_users:
@@ -27,12 +31,12 @@ async def extract_users(msg: Message) -> List[User]:
             except BaseException:
                 pass
 
-    if msg.text:
-        ids = [int(i) for i in msg.text.split() if i.isdigit()]
+    if message.text:
+        ids = [int(i) for i in message.text.split() if i.isdigit()]
         if len(ids):
             for id in ids:
                 try:
-                    total.append(await msg._client.get_users(user_ids=id))
+                    total.append(await message._client.get_users(user_ids=id))
                 except BaseException:
                     pass
 
